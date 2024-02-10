a4f4330e083b88dabc885428a52800a99b5aa729
sarangiperera <150998845+sarangiperera@users.noreply.github.com>
2024-02-10 10:59:30 +0530

    user location dependencies

--- MyApplication/build.gradle.kts
+++ MyApplication/build.gradle.kts
@@ -2,3 +2,15 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
 }
+
+dependencies {
+    implementation 'androidx.appcompat:appcompat:1.6.1'
+    implementation 'com.google.android.material:material:1.11.0'
+    implementation 'com.google.android.gms:play-services-location:19.0.1'
+    implementation 'com.squareup.retrofit2:retrofit:2.9.0'
+    implementation 'com.squareup.retrofit2:converter-gson:2.9.0'
+    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+}
