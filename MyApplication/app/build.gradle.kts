d5f2ec7e09fe2cfa6f709777c75a800cd14059bb
Sankalpa Kulathunga <sankalpakulathunga123@gmail.com>
2024-02-24 13:43:35 +0530

    new

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -37,9 +37,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+<<<<<<< Updated upstream
     implementation("androidx.annotation:annotation:1.7.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+=======
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+>>>>>>> Stashed changes
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
