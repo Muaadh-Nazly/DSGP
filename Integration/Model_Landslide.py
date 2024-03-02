f52350ba2b308da09f28efb03ec75b31fa5ba620
Muaadh Nazly <muaadhnazly@Muaadhs-MacBook-Pro.local>
2024-03-02 17:31:46 +0530

    Added the Integration Parts

---
+++ Integration/Model_Landslide.py
@@ -0,0 +1,61 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+import joblib
+
+# Import models
+rfr_model, ref_columns, target = joblib.load('Landslide_RFRModel.pkl')
+xgb_model, ref_columns, target = joblib.load('Landslide_XGBModel.pkl')
+
+# Read the dictionary for districts and locations
+mapping_district = {}
+with open ('District_Mapped.txt','r+') as district_file:
+    for line in district_file:
+        key,value = line.strip().split(':')
+        mapping_district[key.strip()] = int(value.strip())
+        
+mapping_location = {}
+with open ('Location_Mapped.txt','r+') as district_file:
+    for line in district_file:
+        key,value = line.strip().split(':')
+        mapping_location[key.strip()] = int(value.strip())
+
+# Function to predict the percentage
+def predict_landslide(location,district,month,day,rainfall,location1='None',location2='None'):
+    found= False
+    print(location,location2,location1)
+    locations = [location,location1,location2]
+    for loc in locations:
+        for keys in mapping_location.keys():
+            if loc in keys: # The location may have two words or typo
+                location_code = mapping_location.get(keys)
+                found=True
+                break
+            else:
+                continue
+        if found:
+            break
+    else:
+        location_code = max(mapping_location.values())+1
+    
+    disaster_code = 0
+    district_code = mapping_district.get(district)
+    user_data = pd.DataFrame({
+        'Disaster' : disaster_code,
+        'Location' : location_code,
+        'District' : district_code,
+        'Month' : int(month),
+        'Day' : int(day),
+        'Rainfall(mm)': float(rainfall)
+    },index=[0])
+    rfr_prediction = rfr_model.predict(user_data)*100
+    xgb_prediction = xgb_model.predict(user_data)*100
+    
+    rfr_prediction = np.round(np.clip(rfr_prediction[0],0.01,99.0),2)
+    xgb_prediction = np.round(np.clip(xgb_prediction[0],0.01,99.0),2)
+
+    return rfr_prediction,xgb_prediction
+
+
+
+
--- Integration/app.py
+++ Integration/app.py
@@ -1,54 +1,113 @@
 from flask import Flask,request,jsonify
-import joblib
-import datetime
-import numpy as np
-import pandas as pd
-from Landslide.Model_Landslide import predict_disaster
-
-rfr_model, ref_columns1, target1 = joblib.load('Integration/Flood/Flood_RFRModel.pkl','r+')
-xgb_model, ref_columns2, target2 = joblib.load('Integration/Flood/Flood_XGBModel.pkl','r+')
+import datetime as dt
+from Model_Landslide import predict_landslide
+from Model_Flood import predict_flood
+from Model_Cyclone import predict_cyclone
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello world'
+    return 'IT WORKS'
 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/predict_landslide',methods=['POST'])
+def landslide_predict():
+    
     location = request.form.get('Location')
     location1 = request.form.get('Location1')
     location2 = request.form.get('Location2')
     district = request.form.get('District')
-    month = request.form.get('Month')
-    day = request.form.get('Day')
-    rainfall = request.form.get('Rainfall(mm)')
+    rainfall = float(request.form.get('Rainfall(mm)'))
     
-    rfr_prediction,xgb_prediction = predict_disaster(location,location1,location2,district,month,day,rainfall)
+    date = dt.date.today() 
+    rfr_prediction,xgb_prediction = predict_landslide(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date.month,day=date.day,rainfall=rainfall) 
     
-    rfr_prediction = np.round(rfr_prediction*100,2)
-    xgb_prediction = np.round(xgb_prediction*100,2)
+    def add_day(date, days_to_add):
+        new_date = date + dt.timedelta(days=days_to_add)
+        return new_date
     
-    month = int(month)
-    day = int(day)+1
+    date1 = add_day(date,1)
+    rfr_prediction_day1 , xgb_prediction_day1 = predict_landslide(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date1.month,day=date1.day,rainfall=rainfall) 
     
-    def lastday(month,day):
-        last_day_of_month = (datetime.date(datetime.date.today().year, month % 12 + 1, 1) - datetime.timedelta(days=1)).day
-        if day == last_day_of_month:
-            if month == 12:  
-                month = 1
-            else:
-                month += 1
-                day = 1
-        else:
-            day += 1
-        return month,day
+    date2 = add_day(date,2)
+    rfr_prediction_day2 , xgb_prediction_day2 = predict_landslide(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date2.month,day=date2.day,rainfall=rainfall) 
     
+    date3 = add_day(date,3)
+    rfr_prediction_day3 , xgb_prediction_day3 = predict_landslide(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date3.month,day=date3.day,rainfall=rainfall) 
     
-    print(day)
+    return jsonify({f'Prediction for {date} RFR':float(rfr_prediction),f"Prediction for {date} XGB":float(xgb_prediction),
+                    f'Prediction for {date1} RFR':float(rfr_prediction_day1),f'Prediction for {date1} XGB':float(xgb_prediction_day1),
+                    f'Prediction for {date2} RFR':float(rfr_prediction_day2),f'Prediction for {date2} XGB':float(xgb_prediction_day2),
+                    f'Prediction for {date3} RFR':float(rfr_prediction_day3),f'Prediction for {date3} XGB':float(xgb_prediction_day3)})
+                    
+@app.route('/predict_flood',methods=['POST'])
+def flood_predict():
+    location = request.form.get('Location')
+    location1 = request.form.get('Location1')
+    location2 = request.form.get('Location2')
+    district = request.form.get('District')
+    rainfall = float(request.form.get('Rainfall(mm)'))
     
+    date = dt.date.today() 
+    rfr_prediction,xgb_prediction = predict_flood(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date.month,day=date.day,rainfall=rainfall) 
     
-    return jsonify({'RandomForest Predicted Value':float(rfr_prediction),"XGB Predicted Value":float(xgb_prediction)})
-
+    def add_day(date, days_to_add):
+        new_date = date + dt.timedelta(days=days_to_add)
+        return new_date
+    
+    date1 = add_day(date,1)
+    rfr_prediction_day1 , xgb_prediction_day1 = predict_flood(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date1.month,day=date1.day,rainfall=rainfall) 
+    
+    date2 = add_day(date,2)
+    rfr_prediction_day2 , xgb_prediction_day2 = predict_flood(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date2.month,day=date2.day,rainfall=rainfall) 
+    
+    date3 = add_day(date,3)
+    rfr_prediction_day3 , xgb_prediction_day3 = predict_flood(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date3.month,day=date3.day,rainfall=rainfall) 
+    
+    return jsonify({f'Prediction for {date} RFR':float(rfr_prediction),f"Prediction for {date} XGB":float(xgb_prediction),
+                    f'Prediction for {date1} RFR':float(rfr_prediction_day1),f'Prediction for {date1} XGB':float(xgb_prediction_day1),
+                    f'Prediction for {date2} RFR':float(rfr_prediction_day2),f'Prediction for {date2} XGB':float(xgb_prediction_day2),
+                    f'Prediction for {date3} RFR':float(rfr_prediction_day3),f'Prediction for {date3} XGB':float(xgb_prediction_day3)})
+    
+@app.route('/predict_cyclone',methods=['POST'])
+def cyclone_predict():
+    location = request.form.get('Location')
+    location1 = request.form.get('Location1')
+    location2 = request.form.get('Location2')
+    district = request.form.get('District')
+    wind_speed = float(request.form.get('Wind Speed(mph)'))
+    
+    date = dt.date.today() 
+    rfr_prediction,xgb_prediction = predict_cyclone(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date.month,day=date.day,wind_speed=wind_speed) 
+    
+    def add_day(date, days_to_add):
+        new_date = date + dt.timedelta(days=days_to_add)
+        return new_date
+    
+    date1 = add_day(date,1)
+    rfr_prediction_day1 , xgb_prediction_day1 = predict_cyclone(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date1.month,day=date1.day,wind_speed=wind_speed) 
+    
+    date2 = add_day(date,2)
+    rfr_prediction_day2 , xgb_prediction_day2 = predict_cyclone(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date2.month,day=date2.day,wind_speed=wind_speed) 
+    
+    date3 = add_day(date,3)
+    rfr_prediction_day3 , xgb_prediction_day3 = predict_cyclone(location=location,location1=location1,location2=location2,
+                                                     district=district,month=date3.month,day=date3.day,wind_speed=wind_speed) 
+    
+    return jsonify({f'Prediction for {date} RFR':float(rfr_prediction),f"Prediction for {date} XGB":float(xgb_prediction),
+                    f'Prediction for {date1} RFR':float(rfr_prediction_day1),f'Prediction for {date1} XGB':float(xgb_prediction_day1),
+                    f'Prediction for {date2} RFR':float(rfr_prediction_day2),f'Prediction for {date2} XGB':float(xgb_prediction_day2),
+                    f'Prediction for {date3} RFR':float(rfr_prediction_day3),f'Prediction for {date3} XGB':float(xgb_prediction_day3)})
 if __name__=='__main__':
     app.run(debug=True)
