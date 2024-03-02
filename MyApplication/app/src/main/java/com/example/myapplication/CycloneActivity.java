e10993aca00fd54969f444da4147e7418d0db994
Sankalpa Kulathunga <sankalpakulathunga123@gmail.com>
2024-03-02 21:20:58 +0530

    new

--- MyApplication/app/src/main/java/com/example/myapplication/CycloneActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/CycloneActivity.java
@@ -1,14 +1,41 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
-public class CycloneActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class CycloneActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap gMap;
+    FrameLayout map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cyclone);
+
+        map = findViewById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap = googleMap;
+        LatLng mapIndia = new LatLng(20.5937 , 78.9629);
+        this.gMap.addMarker(new MarkerOptions().position(mapIndia).title("Marker in India"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapIndia));
+
     }
 }
