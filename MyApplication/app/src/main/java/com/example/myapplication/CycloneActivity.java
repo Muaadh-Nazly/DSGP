7d0ff0a43c0e6d8173ba806b4915e2d5e6ff33f5
Sankalpa Kulathunga <sankalpakulathunga123@gmail.com>
2024-03-05 21:54:34 +0530

    new

--- MyApplication/app/src/main/java/com/example/myapplication/CycloneActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/CycloneActivity.java
@@ -1,18 +1,24 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.annotation.NonNull;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class CycloneActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,9 +39,34 @@ public class CycloneActivity extends FragmentActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.gMap = googleMap;
-        LatLng mapIndia = new LatLng(9.6615 , 80.0255);
-        this.gMap.addMarker(new MarkerOptions().position(mapIndia).title("Marker in Jaffna"));
-        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapIndia));
+        LatLng mapSL = new LatLng(7.0840 , 80.0098);
+        Marker marker = this.gMap.addMarker(new MarkerOptions().position(mapSL).title("Marker in Gampaha"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mapSL, 10)); // Adjust the zoom level
 
+        // Set a marker click listener
+        gMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                showBottomSheetDialog();
+                return true;
+            }
+        });
+    }
+
+    private void showBottomSheetDialog() {
+        View view = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+        BottomSheetDialog dialog = new BottomSheetDialog(this);
+        dialog.setContentView(view);
+        // Handle button click
+        Button moreDetailsButton = view.findViewById(R.id.moreDetailsButton);
+        moreDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start another activity to load another XML page
+                Intent intent = new Intent(CycloneActivity.this, More_details.class);
+                startActivity(intent);
+            }
+        });
+        dialog.show();
     }
 }
