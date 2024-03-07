f77cdfe6e6230bfce338113a407eda09e6385ff0
Sankalpa Kulathunga <sankalpakulathunga123@gmail.com>
2024-03-07 12:41:56 +0530

    new

--- MyApplication/app/src/main/java/com/example/myapplication/FloodActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/FloodActivity.java
@@ -1,14 +1,70 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.annotation.NonNull;
 
-public class FloodActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class FloodActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap gMap;
+    FrameLayout map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flood);
+        map = findViewById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap = googleMap;
+        LatLng mapSL = new LatLng(7.0840 , 80.0098);
+        Marker marker = this.gMap.addMarker(new MarkerOptions().position(mapSL).title("Marker in Gampaha"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mapSL, 10)); // Adjust the zoom level
+
+        // Set a marker click listener
+        gMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                showBottomSheetDialog();
+                return true;
+            }
+        });
+    }
+
+    private void showBottomSheetDialog() {
+        View view = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+        BottomSheetDialog dialog = new BottomSheetDialog(this);
+        dialog.setContentView(view);
+        // Handle button click
+        Button moreDetailsButton = view.findViewById(R.id.moreDetailsButton);
+        moreDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start another activity to load another XML page
+                Intent intent = new Intent(FloodActivity.this, More_details.class);
+                startActivity(intent);
+            }
+        });
+        dialog.show();
     }
 }
