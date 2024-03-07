7721c8e4906d54eafc7eacb0fc46d14116f94517
sarangiperera <150998845+sarangiperera@users.noreply.github.com>
2024-03-07 19:41:46 +0530

    updating dependencies

--- MyApplication/build.gradle.kts
+++ MyApplication/build.gradle.kts
@@ -10,7 +10,11 @@ dependencies {
     implementation 'com.squareup.retrofit2:retrofit:2.9.0'
     implementation 'com.squareup.retrofit2:converter-gson:2.9.0'
     implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+    implementation 'com.android.support:multidex:1.0.3'
     testImplementation 'junit:junit:4.13.2'
     androidTestImplementation 'androidx.test.ext:junit:1.1.5'
     androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+    implementation 'com.google.code.gson:gson:2.8.9'
+    implementation 'com.opencsv:opencsv:5.5.2'
+    implementation 'com.google.android.gms:play-services-maps:17.0.1'
 }
